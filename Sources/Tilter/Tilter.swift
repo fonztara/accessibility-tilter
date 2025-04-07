4454992b269d944476fee945c4ce3043fde7ba06
fonztara <fonztara@gmail.com>
2025-04-07 17:34:20 +0200

    Initial Commit

--- Sources/Tilter/Tilter.swift
+++ Sources/Tilter/Tilter.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public struct MyView: View {
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
