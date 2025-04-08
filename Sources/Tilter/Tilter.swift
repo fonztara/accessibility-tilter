0e157f1ea853de97d70864176d5f3d5564a9cf87
fonztara <fonztara@gmail.com>
2025-04-08 16:53:15 +0200

    Initial Commit

--- Sources/Tilter/Tilter.swift
+++ Sources/Tilter/Tilter.swift
@@ -4,19 +4,28 @@
 import SwiftUI
 
 struct AccessibleSlider: ViewModifier {
+    @Binding var isOn: Bool
     @Binding var value: Double
+    
+    var tilterManager: TilterManager {
+        TilterManager(enabled: $isOn, value: $value)
+    }
     
     func body(content: Content) -> some View {
         VStack {
             content
+            Text("\(isOn ? "On" : "Off")")
             Text("\(value)")
+            Toggle(isOn: $isOn) {
+                Text("Toggle")
+            }
         }
     }
 }
 
 extension Slider {
     
-    public func tilterEnabled(value: Binding<Double>) -> some View {
-        return modifier(AccessibleSlider(value: value))
+    public func tilterEnabled(isOn: Binding<Bool>, value: Binding<Double>) -> some View {
+        return modifier(AccessibleSlider(isOn: isOn, value: value))
     }
 }
---
+++ Sources/Tilter/TilterManager.swift
@@ -0,0 +1,142 @@
+//
+//  TilterManager.swift
+//  Tilter
+//
+//  Created by Alfonso Tarallo on 08/04/25.
+//
+
+@preconcurrency import CoreMotion
+@preconcurrency import CoreHaptics
+import SwiftUI
+
+class TilterManager: @unchecked Sendable {
+    let motion = CMMotionManager()
+    
+    var timer: Timer? = nil
+    
+    var devRoll: Double = 0.0
+    var devPitch: Double = 0.0
+    var devYaw: Double = 0.0
+    
+    var enabled: Binding<Bool> {
+        didSet {
+            print("\(enabled.wrappedValue ? "Enabled" : "Disabled")")
+            if enabled.wrappedValue {
+                self.startGyros()
+            } else {
+                self.stopGyros()
+            }
+        }
+    }
+    
+    var value: Binding<Double>
+    
+    init(enabled: Binding<Bool>, value: Binding<Double>) {
+        self.enabled = enabled
+        self.value = value
+    }
+    
+    func startGyros() {
+        let engine = try! CHHapticEngine()
+        engine.playsHapticsOnly = true
+        
+        do {
+            try engine.start()
+        } catch {
+            print("Error starting engine: \(error)")
+        }
+        
+        
+        
+        if motion.isDeviceMotionAvailable {
+            self.motion.deviceMotionUpdateInterval = 1.0 / 10.0
+            self.motion.showsDeviceMovementDisplay = true
+            self.motion.startDeviceMotionUpdates(using: .xArbitraryCorrectedZVertical)
+            
+            var referenceAttitude: CMAttitude? = nil
+            
+            var counter = 0
+            
+            
+            // Configure a timer to fetch the accelerometer data.
+            self.timer = Timer(fire: Date(), interval: (1.0/10.0),
+                               repeats: true, block: { (timer) in
+                // Get the gyro data.
+                if let data = self.motion.deviceMotion {
+                    let roll = data.attitude.roll
+                    let pitch = data.attitude.pitch
+                    let yaw = data.attitude.yaw
+                    
+                    
+                    
+                    guard let referenceAttitude = referenceAttitude else {
+                        referenceAttitude = data.attitude
+                        return
+                    }
+                    
+                    let rollDifference = roll - referenceAttitude.roll
+                    
+                    self.devRoll = round(rollDifference * 180.0 / .pi)
+                    self.devPitch = round(pitch * 180.0 / .pi)
+                    self.devYaw = round(yaw * 180.0 / .pi)
+                    
+                    // Use the gyroscope data in your app.
+                    if counter == 0 {
+                        if (self.devRoll >= 20 && self.devRoll <= 120) {
+                            //MARK: INCREASE
+                            self.increase(by: 0.1)
+                            
+                            do {
+                                let url = Bundle.main.url(forResource: "provaAHAP", withExtension: "json")
+                                try engine.playPattern(from: url!)
+                            } catch {
+                                print("Error playing pattern: \(error)")
+                            }
+                        } else if (self.devRoll <= -20 && self.devRoll >= -120) || (self.devRoll <= 340 && self.devRoll >= 270) {
+                            //MARK: DECREASE
+                            self.decrease(by: 0.1)
+                            
+                            do {
+                                let url = Bundle.main.url(forResource: "provaAHAP", withExtension: "json")
+                                try engine.playPattern(from: url!)
+                            } catch {
+                                print("Error playing pattern: \(error)")
+                            }
+                        }
+                    }
+                    
+                    if counter <= 1 {
+                        counter += 1
+                    } else {
+                        counter = 0
+                    }
+                    
+                }
+            })
+            
+            
+            // Add the timer to the current run loop.
+            RunLoop.current.add(self.timer!, forMode: .default)
+        }
+    }
+    
+    
+    func stopGyros() {
+        if self.timer != nil {
+            self.timer?.invalidate()
+            self.timer = nil
+            
+            
+            self.motion.stopDeviceMotionUpdates()
+        }
+    }
+    
+    func increase(by step: Double) {
+        print("\(self.value.wrappedValue + step)")
+    }
+    
+    func decrease(by step: Double) {
+        print("\(self.value.wrappedValue - step)")
+    }
+    
+}
